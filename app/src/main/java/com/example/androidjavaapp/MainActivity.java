6ae815a90b20d55c0cd2ae970ae083c7fbac64a0
phil <flip.04@mail.ru>
2021-03-21 12:17:55 +0300

    добавлены действия для кнопок с цифрами

--- app/src/main/java/com/example/androidjavaapp/MainActivity.java
+++ app/src/main/java/com/example/androidjavaapp/MainActivity.java
@@ -2,20 +2,51 @@ package com.example.androidjavaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn;
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getViews();
 
+    }
 
+    private void getViews() {
+        tvResult = findViewById(R.id.tvResult);
+    }
+
+    public void btnNumbersOnClick(View btn) {
+        if (btn.getId() == R.id.btnDot && tvResult.getText().toString().contains(getResources().getString(R.string.dot))) {
+            return;
+        }
+        if (tvResult.getText().toString().equals(getResources().getString(R.string._0))
+        && btn.getId() != R.id.btnDot) {
+            tvResult.setText(null);
+        }
+
+        tvResult.setText(tvResult.getText().toString() + ((Button) btn).getText().toString());
+    }
+
+    public void btnClearOnClick(View view) {
+        tvResult.setText(String.valueOf(0));
+    }
+
+    public void btnPlusMinusOnClick(View view) {
+        if (!tvResult.getText().toString().contains(getResources().getString(R.string.minus))) {
+            tvResult.setText(getResources().getString(R.string.minus) + tvResult.getText().toString());
+        } else {
+            tvResult.setText(tvResult.getText().toString().substring(1));
+        }
     }
 }
